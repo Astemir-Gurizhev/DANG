9d1badaaf451a472b086593c91d573611696283e
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2024-11-13 16:05:08 +0300

    added pagination for coin page 50 item

--- src/components/coins/Coins.tsx
+++ src/components/coins/Coins.tsx
@@ -11,6 +11,8 @@ interface Coin {
 const Coins = () => {
   const [coins, setCoins] = useState<Coin[]>([]); 
   const [loading, setLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const coinsPerPage = 50; 
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -28,6 +30,12 @@ const Coins = () => {
     fetchCoins();
   }, []);
 
+  const indexOfLastCoin = currentPage * coinsPerPage;
+  const indexOfFirstCoin = indexOfLastCoin - coinsPerPage;
+  const currentCoins = coins.slice(indexOfFirstCoin, indexOfLastCoin);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   if (loading) {
     return (
       <div className={cn.loader}>
@@ -37,16 +45,43 @@ const Coins = () => {
     );
   }
 
+  const totalPages = Math.ceil(coins.length / coinsPerPage);
+
   return (
     <div className="coins">
       <h2 className={cn.title}>COINS</h2>
       <ul className={cn.list}>
-        {coins.map((coin) => (
+        {currentCoins.map((coin) => (
           <li key={coin.id}>
             {coin.name} ({coin.symbol}): ${coin.price_usd}
           </li>
         ))}
       </ul>
+
+     
+      <div className={cn.pagination}>
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Prev
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index + 1}
+            onClick={() => paginate(index + 1)}
+            className={currentPage === index + 1 ? cn.activePage : ""}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
